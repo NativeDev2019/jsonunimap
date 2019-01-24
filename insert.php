87e6db96e23dcaef696d2ce40a2f015149183266
khirulnizam <kerul76>
2019-01-24 14:07:21 +0800

    create insert.php

---
+++ insert.php
@@ -0,0 +1,39 @@
+<?php
+// code @
+// github.com/khirulnizam/jsonunimap
+//insert.php
+include "connect.php";
+
+$id=$_POST['id'];
+$trainingname=$_POST['trainingname'];
+$website=$_POST['website'];
+$contact=$_POST['contact'];
+$trainingdesc=$_POST['trainingdesc'];
+
+
+$rs = mysqli_query($db,
+	"INSERT INTO a_training 
+	VALUES 
+	('$id','$trainingname', '$website',
+	'$contact','$trainingdesc')");
+
+//echo (mysql_error());
+
+// check for empty result
+
+if ($rs==true) {
+	//insert successfull
+
+		echo "success";
+
+	}//end match found
+
+	else{//no match found
+
+		//send status fail
+
+		echo "error";
+
+	}
+
+?>
